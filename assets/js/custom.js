4d72bb0153b00ea5b9cd997af1c1ad06bdad9020
lilcode-dev <69090574+lilcode-dev@users.noreply.github.com>
2020-12-30 20:19:05 -0400

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -88,33 +88,23 @@ $(document).ready(function(){
 
     // 3.Countdown timer 
         
-        function makeTimer() {
-
-                var endTime = new Date("March 7, 2018 12:00:00 PDT");            
-                var endTime = (Date.parse(endTime)) / 1000;
-
-                var now = new Date();
-                var now = (Date.parse(now) / 1000);
-
-                var timeLeft = endTime - now;
-
-                var days = Math.floor(timeLeft / 86400); 
-                var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-                var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-                var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-
-                if (hours < "10") { hours = "0" + hours; }
-                if (minutes < "10") { minutes = "0" + minutes; }
-                if (seconds < "10") { seconds = "0" + seconds; }
-
-                $("#days").html(days + '<span class="camp">Days</span>');
-                $("#hours").html(hours + '<span class="camp">Hour</span>');
-                $("#minutes").html(minutes + '<span class="camp">Minute</span>');
-                $("#seconds").html(seconds + '<span class="camp">Second</span>');       
-
+        const makeTimer = () =>  {
+            let endTime = ( Date.parse( new Date("January 1, 2021 16:07:53 GMT-0400") ) ) / 1000,
+                now = ( Date.parse( new Date() ) / 1000 ),
+                timeLeft = endTime - now,
+                days = Math.floor(timeLeft / 86400),
+                hours = Math.floor( ( timeLeft - (days * 86400) ) / 3600 ),
+                minutes = Math.floor( ( timeLeft - (days * 86400) - (hours * 3600 )) / 60),
+                seconds = Math.floor( ( timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60) ) );
+            ( hours < "10" ) ? hours = "0".concat(hours) : false;
+            ( minutes < "10" ) ? minutes = "0".concat(minutes) : false;
+            ( seconds < "10" ) ? seconds = "0".concat(seconds) : false;
+            document.querySelector("#days").innerHTML = days + `<span class="camp">Days</span>`;
+            document.querySelector("#hours").innerHTML = hours + `<span class="camp">Hour</span>`;
+            document.querySelector("#minutes").innerHTML = minutes + `<span class="camp">Minute</span>`;
+            document.querySelector("#seconds").innerHTML = seconds + `<span class="camp">Second</span>`;       
         }
-        
-        setInterval(function() { makeTimer(); }, 1000);
+        setInterval( () => makeTimer(), 1000);
 
     // 4. owl carousel
     
